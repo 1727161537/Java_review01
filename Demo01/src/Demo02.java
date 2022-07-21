b60d1100d8f150333455cc1b4563e48ecfd11238
qinhao <1727161537@qq.com>
2022-07-21 17:13:05 +0800

    3th commit

---
+++ Demo01/src/Demo02.java
@@ -0,0 +1,7 @@
+public class Demo02 {
+    public static void main(String[] args) {
+        String a = "你好世界";
+        a = "你好,Java";//字符串的值不能改变,值得改变只是改变了引用对象
+        System.out.println(a);
+    }
+}
