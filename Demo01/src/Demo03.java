dc8a24161778e08a59684f3126293675411c93e4
qinhao <1727161537@qq.com>
2022-07-22 15:43:11 +0800

    Demo01/

--- Demo01/src/Demo03.java
+++ Demo01/src/Demo03.java
@@ -1,14 +1,17 @@
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 
 public class Demo03 {
     public static void main(String[] args) throws IOException {
         //文件输出流
-        FileOutputStream fileOutputStream = new FileOutputStream("D://aaa/b.txt");
-        fileOutputStream.write(80);
-
+        FileOutputStream fileOutputStream = new FileOutputStream("D://aaa/b.txt",true);
+        for (int i = 0; i < 10; i++) {
+            fileOutputStream.write("hello world".getBytes());
+            fileOutputStream.write("\r\n".getBytes());
+        }
         fileOutputStream.close();
     }
 }
