d2ae8886a650a94b54146e112985b94764a00372
qinhao <1727161537@qq.com>
2022-07-26 16:12:48 +0800

    jdbcDemo02/

---
+++ jdbcDemo02/src/main/java/Demo01.java
@@ -0,0 +1,22 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class Demo01 {
+    public static void main(String[] args) throws Exception {
+        //注册驱动
+        Class.forName("com.mysql.jdbc.Driver");
+        //获取连接
+        String url = "jdbc:mysql://localhost:3306/booksystem";
+        String username = "root";
+        String password = "123456";
+        Connection connection = DriverManager.getConnection(url, username, password);
+        //执行sql语句
+        String sql = "update book set bookName ='java' where bookId = 5";
+        Statement statement = connection.createStatement();
+        int i = statement.executeUpdate(sql);
+        System.out.println(i);
+        connection.close();
+        statement.close();
+    }
+}
