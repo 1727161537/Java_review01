b4d5135ebecf297b2940829e31e2e4a6a5691318
qinhao <1727161537@qq.com>
2022-08-11 11:45:46 +0800

    第n+2次提交

---
+++ mybatis_demo01/src/main/java/com/hbue/mapper/BookMapper.java
@@ -0,0 +1,24 @@
+package com.hbue.mapper;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.awt.print.Book;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+public class BookMapper {
+
+    public static void main(String[] args) throws IOException {
+        //加载核心配置文件
+        InputStream inputStream = Resources.getResourceAsStream("mybatis-config.xml");
+        //获取sqlsession工厂对象
+        SqlSessionFactory build = new SqlSessionFactoryBuilder().build(inputStream);
+        //获取sqlsession核心对象
+        SqlSession sqlSession = build.openSession();
+        List<Book> list = sqlSession.selectList("com.hbue.mapper.findAll");`
+    }
+}
